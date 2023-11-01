d49b3978384d6286529c34b7242802490567cb17
Pedrostay <pedrostaay@hotmail.com>
2023-11-01 11:09:32 -0300

    feat: dependencies express from app

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import express from "express";
+import routes from "./router.js";
+
+const app = express();
+app.use(express.json());
+app.use(routes);
+
+app.listen(3000, () => console.log("server is running"));
